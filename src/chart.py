c57ae57693fcb72120854de2be3e958409184e46
Thomas Crescenzi <trescenzi@gmail.com>
2024-06-09 16:35:13 -0400

    break out chart code

---
+++ src/chart.py
@@ -0,0 +1,342 @@
+import cairosvg
+import svgwrite
+import logging
+import itertools
+from typing import List, Dict, NamedTuple
+import psycopg
+from models import (
+    Checkins,
+    Challenges,
+    ChallengeWeeks,
+    Challengers,
+    ChallengerChallenges,
+)
+from peewee import *
+from datetime import datetime, timedelta, date
+import os
+from rule_sets import score
+
+
+connection_string = os.environ["DB_CONNECT_STRING"]
+weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+
+
+class DataUnit(NamedTuple):
+    x: str
+    y: int
+    checkedIn: bool
+    time: datetime
+    tier: str
+
+
+class CheckinChartData(NamedTuple):
+    name: str
+    data: List[DataUnit]
+    totalCheckins: int
+    points: float
+
+    def tostring(self) -> str:
+        return json.dumps({"name": self.name, "data": self.data})
+
+
+def knocked_out(challenge_id):
+    return [
+        n.name
+        for n in Challengers.select(Challengers.name)
+        .join(ChallengerChallenges)
+        .where(
+            (ChallengerChallenges.challenge == challenge_id)
+            & (ChallengerChallenges.knocked_out == True)
+        )
+        .objects()
+    ]
+
+
+def get_names(challenge_id):
+    return [
+        n.name
+        for n in Challengers.select(Challengers.name)
+        .join(ChallengerChallenges)
+        .where(ChallengerChallenges.challenge == challenge_id)
+        .objects()
+    ]
+
+
+def checkin_chart(
+    data: List[CheckinChartData],
+    width: int,
+    height: int,
+    challenge_id,
+    green,
+    bye_week,
+    total_points,
+    achievements,
+):
+    if len(data) == 0:
+        logging.warning("empty week + year selected")
+        dwg = svgwrite.Drawing("empty.svg", size=(10, 10))
+        return dwg.tostring()
+
+    wGap = 0
+    hGap = 20
+    gutter = 85
+    colors = ["#f7f7f7", "#cccccc", "#969696", "#636363", "#252525"]
+    greens = [
+        "#edf8e9",
+        "#c7e9c0",
+        "#a1d99b",
+        "#74c476",
+        "#41ab5d",
+        "#238b45",
+        "#005a32",
+    ]
+    green_mode = greens[4]
+    base_color = green_mode if green else "white"
+
+    columns = len(data)
+    rows = len(data[0].data)
+    rectW = (width - rows * wGap - gutter) / (rows + 1)
+    rectH = (height - columns * hGap - gutter) / (columns)
+
+    dwg = svgwrite.Drawing("checkin.svg", size=(width + 1, height), debug=False)
+    dwg.add(
+        dwg.rect(
+            insert=(0, 0),
+            size=("100%", "100%"),
+            fill="white" if not green else green_mode,
+        )
+    )
+    knocked_out_names = knocked_out(challenge_id)
+    logging.info("knocked out: %s", knocked_out_names)
+    text_color = "black" if green else ""
+    for column, chart in enumerate(data):
+        yLabel = chart.name
+        is_knocked_out = yLabel in knocked_out_names
+        text1 = dwg.text(
+            yLabel,
+            insert=(0, rectH * column + hGap * column + gutter + rectH / 2),
+            font_size=14,
+            font_weight="bold" if not is_knocked_out else "normal",
+            text_decoration="line-through" if is_knocked_out else "none",
+            fill=text_color,
+        )
+        dwg.add(text1)
+        for row, dataUnit in enumerate(chart.data):
+            x = dataUnit.x
+            checkedIn = dataUnit.checkedIn
+            fill_color = colors[2] if checkedIn and not is_knocked_out else base_color
+            fill_color = colors[0] if is_knocked_out and checkedIn else fill_color
+            stroke_color = colors[3] if not is_knocked_out else colors[1]
+
+            if chart.totalCheckins >= 5 and dataUnit.y != 0:
+                fill_color = greens[4] if not green else greens[6]
+            if chart.totalCheckins >= 5:
+                stroke_color = greens[6]
+            # gold for 7!
+            if chart.totalCheckins >= 7:
+                fill_color = "#D4AF37"
+            # lime for first to five
+            if (
+                achievements[2] is not None
+                and chart.totalCheckins >= 5
+                and chart.name == achievements[2][0]
+                and dataUnit.time == achievements[2][1]
+            ):
+                fill_color = "#39FF14"
+
+            if column == 0:
+                # add day of week
+                text = dwg.text(x, fill=text_color)
+                text.translate(
+                    rectW * row + wGap * row + gutter + rectW / 2, gutter - 10
+                )
+                text.rotate(-90)
+                dwg.add(text)
+
+            rect = dwg.rect(
+                insert=(
+                    row * rectW + row * wGap + gutter,
+                    column * rectH + column * hGap + gutter,
+                ),
+                size=(rectW, rectH),
+                fill=fill_color,
+                stroke=stroke_color,
+                stroke_width=1,
+                rx=2,
+                ry=2,
+            )
+            group = dwg.g()
+            text = dwg.text(dataUnit.tier)
+            text.translate(
+                row * rectW + row * wGap + gutter + rectW / 2 - 5,
+                column * rectH + column * hGap + gutter + rectH / 2 + 5,
+            )
+            group.add(rect)
+            if dataUnit.tier:
+                group.add(text)
+            if (
+                dataUnit.time is not None
+                and dataUnit.time.strftime("%H:%M") == achievements[1]
+            ):
+                text = dwg.text("🌚")
+                text.translate(
+                    row * rectW + row * wGap + gutter + rectW / 2 + 15,
+                    column * rectH + column * hGap + gutter + rectH / 2 + 5,
+                )
+                group.add(text)
+            if (
+                dataUnit.time is not None
+                and dataUnit.time.strftime("%H:%M") == achievements[0]
+            ):
+                text = dwg.text("🌞")
+                text.translate(
+                    row * rectW + row * wGap + gutter + rectW / 2 + 15,
+                    column * rectH + column * hGap + gutter + rectH / 2 + 5,
+                )
+                group.add(text)
+
+            dwg.add(group)
+
+        if chart.name in total_points:
+            logging.info(
+                "adding points for %s total %s week %s",
+                chart.name,
+                total_points[chart.name],
+                chart.points,
+            )
+            text = dwg.text(
+                "%.1f (%.1f)" % (round(chart.points, 1), total_points[chart.name])
+            )
+            text.translate(
+                rows * rectW + rows * wGap + gutter + rectW / 2 - 30,
+                column * rectH + column * hGap + gutter + rectH / 2,
+            )
+            dwg.add(text)
+
+    # Add Points Label
+    text = dwg.text("Points", fill=text_color)
+    text.translate(
+        rectW * (rows) + wGap * (rows) + gutter + rectW / 2 - 30,
+        gutter - 30,
+    )
+    dwg.add(text)
+    text = dwg.text("(Total)", fill=text_color)
+    text.translate(
+        rectW * (rows) + wGap * (rows) + gutter + rectW / 2 - 30,
+        gutter - 10,
+    )
+    dwg.add(text)
+
+    if bye_week:
+        text = dwg.text("BYE")
+        text.translate(width / 4, height / 2)
+        text["font-size"] = 200
+        text.fill = text_color
+        dwg.add(text)
+        text = dwg.text("WEEK")
+        text.translate(width / 4 - 50, height / 2 + 175)
+        text["font-size"] = 200
+        text.fill = text_color
+        dwg.add(text)
+
+    return dwg.tostring()
+
+
+def write_og_image(svg, week):
+    try:
+        output = "./static/preview-{week}.png".format(week=week)
+        cairosvg.svg2png(bytestring=svg.encode("utf-8"), write_to=output)
+    except:
+        logging.info("Failed to write og image")
+
+
+def sortCheckinByWeekday(data: List[str]) -> List[str]:
+    return sorted(data, key=lambda x: weekdays.index(x.day_of_week))
+
+
+def week_heat_map_from_checkins(checkins, challenge_id, rule_set):
+    heatmap_data = []
+    latest_date = None
+    with psycopg.connect(conninfo=connection_string) as conn:
+        with conn.cursor() as cur:
+            cur.execute("select time from checkins order by time desc limit 1")
+            latest_date = cur.fetchone()
+        conn.commit()
+    checkins.sort(key=lambda x: x.name)
+    weeks_grouped_by_name = {
+        name: list(value)
+        for name, value in itertools.groupby(checkins, key=lambda x: x.name)
+    }
+
+    names = get_names(challenge_id)
+    logging.info("Challengers: %s", names)
+    for name in names:
+        if name not in weeks_grouped_by_name:
+            weeks_grouped_by_name[name] = []
+
+    latest = "00:00:00"
+    earliest = "23:59:59"
+    first_to_five = None
+    if len(checkins) > 0:
+        last_checkin = sorted(checkins, key=lambda x: x.time, reverse=True)[0]
+        first_to_five = (last_checkin.name, last_checkin.time)
+    for name in weeks_grouped_by_name:
+        sorted_checkins = sortCheckinByWeekday(weeks_grouped_by_name[name])
+        data = []
+        total_checkins = 0
+        point_checkins = []
+        for i, weekday in enumerate(weekdays):
+            checkinIndex = next(
+                (
+                    index
+                    for index, checkin in enumerate(sorted_checkins)
+                    if checkin.day_of_week == weekday
+                ),
+                -1,
+            )
+            tier = (
+                sorted_checkins[checkinIndex].tier
+                if len(sorted_checkins) > checkinIndex and checkinIndex >= 0
+                else None
+            )
+            time = (
+                sorted_checkins[checkinIndex].time
+                if len(sorted_checkins) > checkinIndex and checkinIndex >= 0
+                else None
+            )
+            time_hour = time.strftime("%H:%M") if time else None
+            checked_in = bool(checkinIndex + 1)
+            if time_hour and time_hour > latest:
+                latest = time_hour
+            if time_hour and time_hour < earliest:
+                earliest = time_hour
+            total_checkins += 1 if checked_in else 0
+            if (
+                first_to_five is not None
+                and total_checkins > 4
+                and time is not None
+                and time < first_to_five[1]
+            ):
+                logging.info("new first to five %s %s", name, time)
+                first_to_five = (name, time)
+            if tier:
+                point_checkins.append(score(tier, rule_set))
+            data.append(DataUnit(weekday, checkinIndex + 1, checked_in, time, tier))
+        heatmap_data.append(
+            CheckinChartData(
+                name,
+                data,
+                total_checkins,
+                sum(sorted(point_checkins, reverse=True)[:5]),
+            )
+        )
+    return heatmap_data, latest_date[0], (earliest, latest, first_to_five)
+
--- src/rule_sets.py
+++ src/rule_sets.py
@@ -9,16 +9,19 @@ from peewee import *
 import itertools
 import os
 import logging
+
 LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
 logging.basicConfig(level="INFO")
 pwlogger = logging.getLogger("peewee")
 pwlogger.addHandler(logging.StreamHandler())
 pwlogger.setLevel(logging.DEBUG)
 
+
 def score(tier, rule_set):
     if rule_set == 1:
         return version_1_score(tier)
     return version_2_score(tier)
+
 
 def version_1_score(tier):
     match tier:
@@ -34,22 +37,34 @@ def version_1_score(tier):
             return 1.5
     return 1
 
+
 def version_2_score(tier):
     if tier == "T0":
         return 0
     number = int(tier.lstrip("T"))
-    points = 0.9+0.1*number
+    points = 0.9 + 0.1 * number
     logging.info("Tier: %s Number: %s Points: %s", tier, number, points)
     return points
 
+
 def calculate_total_score(challenge_id):
     checkins_this_challenge = (
-        Checkins.select(fn.Max(Checkins.tier), Checkins.name, Checkins.challenge_week, Challenges.rule_set)
+        Checkins.select(
+            fn.Max(Checkins.tier),
+            Checkins.name,
+            Checkins.challenge_week,
+            Challenges.rule_set,
+        )
         .join(ChallengeWeeks, on=((Checkins.challenge_week == ChallengeWeeks.id)))
         .where(ChallengeWeeks.challenge == challenge_id)
         .join(Challenges, on=((Challenges.id == ChallengeWeeks.challenge)))
         .where(Challenges.id == challenge_id)
-        .group_by(fn.date(Checkins.time), Checkins.name, Checkins.challenge_week, Challenges.rule_set)
+        .group_by(
+            fn.date(Checkins.time),
+            Checkins.name,
+            Checkins.challenge_week,
+            Challenges.rule_set,
+        )
         .order_by(Checkins.challenge_week)
         .objects()
     )
@@ -77,8 +92,10 @@ def calculate_total_score(challenge_id):
                     )[:5]
                 ),
                 4,
-            ) for week in weeks
-        ) for n in names
+            )
+            for week in weeks
+        )
+        for n in names
     }
     logging.info("Total Points: %s", result)
     return result
