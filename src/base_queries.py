3ce322cf75f56be6b55cffa1627fd97918299848
Thomas Crescenzi <trescenzi@gmail.com>
2024-09-28 15:14:07 -0400

    refactor: update remaining queries to use helpers

--- src/base_queries.py
+++ src/base_queries.py
@@ -1,13 +1,6 @@
 from helpers import fetchone, fetchall
 from datetime import datetime, timedelta, date
-import psycopg
-import os
 import itertools
-import logging
-
-connection_string = os.environ["DB_CONNECT_STRING"]
-LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
-logging.basicConfig(level="DEBUG")
 
 
 def points_so_far(challenge_id):
@@ -40,21 +33,15 @@ def total_ante(challenge_id, tier):
 
 def total_possible_checkins_so_far(challenge_id, week_id):
     sql = "select count(*) * 5 as total_possible from challenge_weeks where challenge_id = %s and id < %s;"
-    with psycopg.connect(conninfo=connection_string) as conn:
-        with conn.cursor() as cur:
-            cur.execute(sql % (challenge_id, week_id))
-            checkins_possible_before_now = cur.fetchone()[0]
-            now = datetime.now()
-            day_of_week = now.weekday()
-            return checkins_possible_before_now + min(day_of_week + 1, 5)
+    checkins_possible_before_now = fetchone(sql, (challenge_id, week_id))[0]
+    now = datetime.now()
+    day_of_week = now.weekday()
+    return checkins_possible_before_now + min(day_of_week + 1, 5)
 
 
 def total_possible_checkins(challenge_id):
     sql = "select count(*) * 5 as total_possible from challenge_weeks where challenge_id = %s;"
-    with psycopg.connect(conninfo=connection_string) as conn:
-        with conn.cursor() as cur:
-            cur.execute(sql % challenge_id)
-            return cur.fetchone()
+    return fetchone(sql, [challenge_id])
 
 
 def challenge_weeks():
