254aed46cffbaf9111926d82141aaade1ec447cc
Thomas Crescenzi <trescenzi@gmail.com>
2024-09-28 14:40:40 -0400

    refactor: break out queries

---
+++ src/base_queries.py
@@ -0,0 +1,121 @@
+from helpers import fetchone, fetchall
+from datetime import datetime, timedelta, date
+import psycopg
+import os
+import itertools
+import logging
+
+connection_string = os.environ["DB_CONNECT_STRING"]
+LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
+logging.basicConfig(level="DEBUG")
+
+
+def points_so_far(challenge_id):
+    with psycopg.connect(conninfo=connection_string) as conn:
+        with conn.cursor() as cur:
+            cur.execute("select * from get_challenge_score(%s, FALSE)", [challenge_id])
+            return cur.fetchall()
+
+
+def get_challenges():
+    with psycopg.connect(conninfo=connection_string) as conn:
+        with conn.cursor() as cur:
+            logging.debug("select * from challenges")
+            cur.execute("select * from challenges")
+            return cur.fetchall()
+
+
+def bi_checkins(challenge_id):
+    sql = "select sum(bi_checkins) from challenger_challenges where challenge_id = %s"
+    return fetchone(sql, [challenge_id]).sum
+
+
+def points_knocked_out(challenge_id):
+    with psycopg.connect(conninfo=connection_string) as conn:
+        with conn.cursor() as cur:
+            cur.execute("select * from get_challenge_score(%s, TRUE)", [challenge_id])
+            return cur.fetchall()
+
+
+def challenge_data(challenge_id):
+    with psycopg.connect(conninfo=connection_string) as conn:
+        with conn.cursor() as cur:
+            cur.execute("select * from challenges where id = %s;" % challenge_id)
+            return cur.fetchone()
+
+
+def total_ante(challenge_id, tier):
+    sql = "select sum(ante) from challenger_challenges where challenge_id = %s and tier = %s"
+    with psycopg.connect(conninfo=connection_string) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, (challenge_id, tier))
+            return cur.fetchone()[0]
+
+
+def total_possible_checkins_so_far(challenge_id, week_id):
+    sql = "select count(*) * 5 as total_possible from challenge_weeks where challenge_id = %s and id < %s;"
+    with psycopg.connect(conninfo=connection_string) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql % (challenge_id, week_id))
+            checkins_possible_before_now = cur.fetchone()[0]
+            now = datetime.now()
+            day_of_week = now.weekday()
+            return checkins_possible_before_now + min(day_of_week + 1, 5)
+
+
+def total_possible_checkins(challenge_id):
+    sql = "select count(*) * 5 as total_possible from challenge_weeks where challenge_id = %s;"
+    with psycopg.connect(conninfo=connection_string) as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql % challenge_id)
+            return cur.fetchone()
+
+
+def challenge_weeks():
+    sql = """
+        select c.name, cw.id, cw.start from challenge_weeks cw
+        join challenges c on cw.challenge_id = c.id
+        order by cw.start
+        """
+    challenges = fetchall(sql, [])
+    return [
+        list(value) for n, value in itertools.groupby(challenges, key=lambda x: x.name)
+    ]
+
+
+def get_current_challenge_week(tz="America/New_York"):
+    sql = """
+        select * from challenge_weeks 
+        where 
+            week_of_year = extract(week from current_timestamp at time zone %s) and
+            extract(year from start) = extract(year from current_date)
+        """
+    return fetchone(sql, [tz])
+
+
+def get_current_challenge():
+    return fetchone(
+        'select * from challenges where start <= CURRENT_DATE and "end" >= CURRENT_DATE'
+    )
+
+
+def checkins_this_week(challenge_week_id):
+    sql = """
+    select
+      ch.name, checkins.day_of_week, c.tier, c.time at time zone ch.tz as time, cw.bye_week
+    from
+      (select day_of_week, max(ltrim(tier, 'T')::INT) as max_tier, challenger
+       from checkins
+       where challenge_week_id = %s
+       group by day_of_week, challenger) as checkins
+    join
+      checkins c on c.day_of_week = checkins.day_of_week and ltrim(c.tier, 'T')::INT = checkins.max_tier
+    join
+      challenge_weeks cw on cw.id = c.challenge_week_id
+    join
+      challengers ch on ch.id = c.challenger
+    where cw.id = %s
+    group by ch.name, checkins.day_of_week, c.tier, c.time, cw.bye_week, ch.tz
+    order by day_of_week, time desc;
+    """
+    return fetchall(sql, (challenge_week_id, challenge_week_id))
