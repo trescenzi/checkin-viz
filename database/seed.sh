9b01543e3565f53d781c5b82e78cd21bbbd3f46a
Thomas Crescenzi <t@tcrez.dev>
2026-07-28 13:30:51 -0500

    chore: fix decryption

--- database/seed.sh
+++ database/seed.sh
@@ -1,14 +1,28 @@
 #! /bin/bash
 
+echo "------------------------------------------"
+echo "-------- Dropping Existing Data ----------"
+echo "------------------------------------------"
+PGPASSWORD=password psql -h postgres -U postgres -c "
+  DROP SCHEMA public CASCADE;
+  CREATE SCHEMA public;
+  GRANT ALL ON SCHEMA public TO postgres;
+  GRANT ALL ON SCHEMA public TO PUBLIC;
+"
+
+echo "------------------------------------------"
 echo "------------ Creating Tables -------------"
+echo "------------------------------------------"
 # -h postgres works because the service's name is postgres
 # and we're assuming this runs in docker
-PGPASSWORD=password pg_restore -d projects -h postgres -U postgres -cCO /seed/schema.dump
+PGPASSWORD=password pg_restore -h postgres -U postgres --no-owner --clean --create -f /seed/schema.dump
 
+echo "------------------------------------------"
 echo "------- Beginning Database Seeding -------"
 echo "------------------------------------------"
 
-psql $LOCAL_DB_CONNECT_STRING -f /seed/load.sql
+PGPASSWORD=password psql $LOCAL_DB_CONNECT_STRING -f /seed/load.sql
 
+echo "------------------------------------------"
 echo "--------- Done Database Seeding ----------"
 echo "------------------------------------------"
